69321ecef34d65f8f6c1bd82d5c8dbaca3d3fd69
Áron Málnai <aron.malnai@stud.uni-corvinus.hu>
2021-10-08 10:03:02 +0200

    merge week/a4 into dev

--- UserMaintenance/week04/Form1.cs
+++ UserMaintenance/week04/Form1.cs
@@ -10,11 +10,32 @@ using System.Windows.Forms;
 
 namespace week04
 {
-    public partial class Form1 : Form
+    public partial class Form1 : RealEstateEntities
+
+
     {
+
+
+        RealEstateEntities context = new RealEstateEntities();
+        List<Flat> Flats;
+
+
+
+
         public Form1()
         {
             InitializeComponent();
+            LoadData();
+            //using Excel = Microsoft.Office.Interop.Excel;
+            //using system.Reflection;
+        }
+
+        private void LoadData()
+        {
+
+            Flats = context.Flats.ToList();
+            
+
         }
     }
 }
