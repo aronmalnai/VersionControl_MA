eee1730ede725da0b0b811812e8581ed08a399c6
Áron Málnai <aron.malnai@stud.uni-corvinus.hu>
2021-11-23 16:38:40 +0100

    email validation part2

--- week08/UnitTestExample.Test/AccountControllerTestFixture.cs
+++ week08/UnitTestExample.Test/AccountControllerTestFixture.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UnitTestExample.Controllers;
 
 namespace UnitTestExample.Test
 {
@@ -11,11 +12,15 @@ namespace UnitTestExample.Test
     {
         [Test]
         public void TestValidateEmail(string email, bool expectedResult)
-        { 
-        
-        
-        
-        
+        {
+            var accountController = new AccountController();
+
+            var actualResult = accountController.ValidateEmail(email);
+            Assert.AreEqual(expectedResult, actualResult);
+
+
+
+
         }
 
 
